040774444ea64c2b2976077cc507bd474741cc1d
Babacarlo99 <77294364+Babacarlo99@users.noreply.github.com>
2021-01-12 04:08:20 +0000

    Add files via upload

---
+++ carre.php
@@ -0,0 +1,39 @@
+#Exercice CARREE
+<?php
+#declarons et Initialisons
+
+#Cote du carre
+define("COTE", 2);
+echo cote;
+
+#perimetre
+$cote1=2;
+$cote2=2;
+$cote3=2;
+$cote4=2;
+echo (cote1 + cote2 + cote3 + cote4);
+
+#demi perimetre
+$cote1=2;
+$cote2=2;
+echo (longueur + largeur);
+
+#Surface
+$cote1=2;
+$cote2=2;
+echo (cote*cote);
+
+
+#diagonal
+
+
+
+
+
+
+
+
+
+
+
+?>
