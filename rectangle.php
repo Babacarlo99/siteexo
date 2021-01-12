041450fcf80fb7bb985bb9d787db6c50b7294a1c
Babacarlo99 <77294364+Babacarlo99@users.noreply.github.com>
2021-01-12 04:12:05 +0000

    Add files via upload

---
+++ rectangle.php
@@ -0,0 +1,28 @@
+
+<?php>
+#Rectangle
+#declarons
+$longueur= 5;
+echo $longueur;
+$largeur =4;
+echo $largeur;
+#perimetre
+$longueur= 5;
+echo $longueur;
+$largeur =4;
+echo ($longeur + $largeur) * 2;
+
+#demi perimetre
+$longueur=5;
+$largeur=4;
+echo (longueur + largeur);
+
+
+#Surface
+$longueur=5;
+$largeur=4;
+echo ($longueur*$largeur)
+
+#Diagonal
+
+?>
