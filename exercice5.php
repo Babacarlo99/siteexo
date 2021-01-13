397d515b2fdacac257c3ebe5ca4cdf111c41cfc5
Babacarlo99 <77294364+Babacarlo99@users.noreply.github.com>
2021-01-13 17:05:56 +0000

    Add files via upload

---
+++ exercice5.php
@@ -0,0 +1,38 @@
+Initialiser une variable une annee  entier
+      puis affiche si cette année est bisextile ou pas 
+  */
+  /*
+    Exercice 5:
+     Initialiser une variable un mois et une  annee qui sont 
+     des  entiers puis affiche determine le nombre de jours 
+     de ce mois dans cette annee
+     Exemple :
+     mois=2 annee:2020  nbreJours=29
+     mois=2 annee:2021  nbreJours=28
+     mois=12 annee:2021  nbreJours=31
+     mois=4 annee:2021  nbreJours=30
+
+<?php
+$mois=rand(1,12);
+$anne=rand(2010,2021);
+switch ($mois,$anne);{
+         case 1=;
+         echo"$mois,$anne nbre de jours=29";
+         break;
+}          
+case 1=;
+echo"$mois,$anne nbre de jours=28";
+break;
+}          
+case 1=;
+echo"$mois,$anne nbre de jours=31";
+break;
+}          
+case 1=;
+echo"$mois,$anne nbre de jours=30";
+break;
+}          
+
+
+
+?>     
